51e3ce7548bef49538744fd78d0946cebc642c39
l7ssha <szymon.uglis@hotmail.com>
2018-01-08 21:19:34 +0100

    Dummy file

---
+++ src/tagInserter.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+
+export async function insertTag() {
+    const tag = await vscode.window.showInputBox();
+    if(tag == null && tag.trim() != "")
+        return;
+
+    let selection = vscode.window.activeTextEditor.selection;
+
+    if(!selection.isEmpty) {
+        vscode.window.activeTextEditor.edit(builder => {
+            builder.replace(selection, getInsertText(vscode.window.activeTextEditor.document.getText(selection), tag));
+        });
+    }
+};
+
+export function getInsertText(selection: string, tag: string) {
+    if(tag.includes("=\"") == true)
+    {
+        const t = tag.split(" ", 1);
+        return "<" + tag +">" + selection + "</" + t[0] + ">";
+    }
+    else
+        return "<" + tag +">" + selection + "</" + tag + ">";
+}
